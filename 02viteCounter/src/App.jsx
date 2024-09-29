4958ccafa15ea0c9d351975b40a8807bc1cacc5e
Ashish Yadav <yashishkumar@google.com>
2024-09-29 16:04:46 +0000

    updaing the counter app and finalize the vite-telwind app.

--- 02viteCounter/src/App.jsx
+++ 02viteCounter/src/App.jsx
@@ -3,8 +3,23 @@ import { useState } from "react"
 function App() {
   const [counter , setCounter] = useState(0);
 
-  function increment(){
-    setCounter(counter+1);
+  // function increment(){
+  // // it will not increase counter + 6 due to taking the whole value as a bunch and update them.
+  //   setCounter(counter+1);
+  //   setCounter(counter+1);
+  //   setCounter(counter+1);
+  //   setCounter(counter+1);
+  //   setCounter(counter+1);
+  //   setCounter(counter+1);
+  // };
+  
+  function increment(){ 
+    // it will increase counter + 5 due to taking value from previous itteration and update them.
+    setCounter(preCounter=>preCounter+1);
+    setCounter(preCounter=>preCounter+1);
+    setCounter(preCounter=>preCounter+1);
+    setCounter(preCounter=>preCounter+1);
+    setCounter(preCounter=>preCounter+1);
   };
 
   function decrement(){
