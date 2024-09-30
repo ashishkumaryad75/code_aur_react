47fe4bd8af82a8c5408e7d4b5621c9f090c55c63
Atul Kumar Yadav <ashish@192.168.1.5>
2024-09-30 14:58:45 +0530

    password generator added.

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import PassGen from './components/PassGen';
+
+function App() {
+
+  return (
+    
+    <>
+      <h1 className="text-4xl text-center text-white">Password Generator...</h1>
+      <PassGen />
+    </>
+  )
+}
+
+export default App
