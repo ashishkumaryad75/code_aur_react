0f4ae9b54be2399703525b40ba76eca5dbf5f2de
Atul Kumar Yadav <ashish@192.168.1.6>
2024-09-30 11:26:22 +0530

    bg changer updated.

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -10,12 +10,24 @@ const [color,setColor]= useState('white');
         </h3>
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"red"}} onClick={()=>setColor('red')}>Red</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"green"}} onClick={()=>setColor('green')}>Green</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"blue"}} onClick={()=>setColor('blue')}>Blue</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"olive"}} onClick={()=>setColor('olive')}>Olive</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"black"}} onClick={()=>setColor('black')}>Black</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' style={{backgroundColor:"yellow"}} onClick={()=>setColor('yellow')}>Yellow</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"red"}} onClick={()=>setColor('red')}>Red</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"lightblue"}} onClick={()=>setColor('lightblue')}>Sky Blue</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"orange"}} onClick={()=>setColor('orange')}>Orange</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"green"}} onClick={()=>setColor('green')}>Green</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"blue"}} onClick={()=>setColor('blue')}>Blue</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"olive"}} onClick={()=>setColor('olive')}>Olive</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"black"}} onClick={()=>setColor('black')}>Black</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"yellow"}} onClick={()=>setColor('yellow')}>Yellow</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white font-semibold text-lg' 
+              style={{backgroundColor:"violet"}} onClick={()=>setColor('violet')}>Violet</button>
           </div>
         </div>
       </div>
