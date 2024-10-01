78960a1919826f8585f2fb69b74778b00a296201
Atul Kumar Yadav <ashish@192.168.1.3>
2024-10-01 10:06:04 +0530

    besic setup for react router.

---
+++ 07reactRounter/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App
